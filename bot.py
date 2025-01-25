2772a958e43e8795ab73bce7fa3148511ffddebe
Luiz Gustavo <luizg0509@gmail.com>
2025-01-24 21:02:26 -0300

    Handeling errors

--- bot.py
+++ bot.py
@@ -13,42 +13,48 @@ API_TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 bot = telebot.TeleBot(API_TOKEN)
 
-# ms = defaultdict(list)
-
 
 url_s = os.getenv("URL_SUPA")
 key_s = os.getenv("KEY_SUPA")
 
 try:
     supabase = create_client(url_s, key_s)
-except:
+
+except Exception:
     print("Erro ao conectar ao banco de dados")
     sys.exit(1)
 
 print("Iniciando bot")
 
+
 # TODO: Adicionar coisas de tratamento de erro
 def addLink(user, link):
     supabase.table("links").insert({"url": link, "user_id": user}).execute()
 
+
 def getLinks(user):
     r = supabase.table("links").select("*").eq("user_id", user).execute()
-    # exclui 
-    supabase.table("links").delete().eq("user_id", user).execute()
+    # exclui
+    # supabase.table("links").delete().eq("user_id", user).execute()
     return r.data
 
-@bot.message_handler(commands=['ola', 'start'])
+
+@bot.message_handler(commands=["ola", "start"])
 def send_welcome(message):
-    bot.reply_to(message, "Mande links que serão guardados e use o comanod /links ou /l para retornar todos eles(serão apagados depois)")
+    bot.reply_to(
+        message,
+        "Mande links que serão guardados e use o comanod /links ou /l para retornar todos eles(serão apagados depois)",
+    )
 
 
-@bot.message_handler(commands=['links', 'l'])
+@bot.message_handler(commands=["links", "l"])
 def a(m):
     r = getLinks(m.chat.id)
     urls = [i["url"] for i in r]
 
     if len(urls) == 0:
         bot.send_message(m.chat.id, "Sem links guardados")
+        return
     else:
         bot.send_message(m.chat.id, "Links selecionados:")
         for i in urls:
@@ -58,15 +64,18 @@ def a(m):
     print(f"Arquivo {temp_file} criado")
     processList(urls, temp_file)
 
-    with open(temp_file, "r") as f:
-        bot.send_document(m.chat.id, f)
+    try:
+        with open(temp_file, "r") as f:
+            bot.send_document(m.chat.id, f)
 
-    try: 
         os.remove(temp_file)
-    except:
+    except Exception:
         print(f"Erro ao remover {temp_file}")
 
-link_r = r'https?:\/\/[^\s/$.?#].[^\s]*'
+
+link_r = r"https?:\/\/[^\s/$.?#].[^\s]*"
+
+
 @bot.message_handler(func=lambda m: True)
 def link_add(m):
     if re.search(link_r, m.text):
@@ -74,5 +83,6 @@ def link_add(m):
         bot.reply_to(m, "Link adicionado")
     else:
         bot.reply_to(m, "Não foi reconhecido como um link")
+
 
 bot.infinity_polling()
--- scrapper.py
+++ scrapper.py
@@ -7,13 +7,18 @@ import html2text as htmd
 from datetime import date
 import sys
 
+
 def generate_file_td():
     return date.today().strftime("%d_%m")
 
+
 def generate_random_filename(extension="html", length=8):
     letters = string.ascii_lowercase
-    random_filename = ''.join(random.choice(letters) for _ in range(length)) + f".{extension}"
+    random_filename = (
+        "".join(random.choice(letters) for _ in range(length)) + f".{extension}"
+    )
     return random_filename
+
 
 def getHtmlFromX(tweet_url):
     # URL base e parâmetros
@@ -34,21 +39,31 @@ def getHtmlFromX(tweet_url):
     params = {"url": tweet_url}
 
     # Fazer a requisição
+    soup = getSoap(base_url, params, headers)
+    if soup == -1:
+        return -1
+
+    # TODO: Pegar videos
+    content = soup.find("div", {"id": "thread"})
+    if content:
+        return writeHtml(tweet_url, str(content.prettify()))
+    else:
+        return -1
+
+
+def getSoap(url="", params={}, headers={}):
     try:
-        response = requests.get(base_url, headers=headers, params=params, timeout=10)
-        response.raise_for_status()  # Levanta exceções para códigos de erro HTTP
-        
-        soup = BeautifulSoup(response.text, "html.parser")
-        
-        # TODO: Pegar videos
-        content =  soup.find("div", {"id": "thread"})
-        if content:
-            # print(content)
-            return writeHtml(tweet_url, str(content.prettify()))
+        r = requests.get(url, headers=headers, params=params, timeout=10)
+        r.raise_for_status()
+
+        soup = BeautifulSoup(r.text, "html.parser")
+        if soup:
+            return soup
         else:
             return -1
     except requests.exceptions.RequestException:
         return -1
+
 
 def saveMd(ori, des, prefix=""):
     with open(ori, "r") as f:
@@ -60,103 +75,110 @@ def saveMd(ori, des, prefix=""):
             m.write(prefix)
             m.write(str(l).replace("* * *", "\n"))
 
+
 def writeHtml(url, content):
-   random_filename = generate_random_filename()
-   with open(random_filename, "w", encoding="utf-8") as html_file:
+    random_filename = generate_random_filename()
+    with open(random_filename, "w", encoding="utf-8") as html_file:
         html_file.write(f"<a href='{url}'>Link</a>\n")
         html_file.write(content)
-    
-   return random_filename
 
+    return random_filename
 
 
 def getHtmlFromReddit(reddit_url):
-    try:
-        r = requests.get(reddit_url)
-        r.raise_for_status()
-        soup = BeautifulSoup(r.text, "html.parser")
-        
-        # TODO: Pegar videos
-        all = soup.find("shreddit-post")
-        _author = all.get("author", "Autor Desconhecido") if all else "Autor Desconhecido"
-        soup.find("div", {"slot": "credit-bar"}).decompose()
-
-        # Elimina elementos que só carregam mais tarde
-        for tag in soup.find_all(attrs={"loading": "lazy"}):
-            tag.decompose()  # Remove a tag do documento
-        seen_src = set()
-        # Ao pegar posts apenas com imagens ele repete as imagens por causa de navegabilidade...
-        # assim basicamente estou deixando apenas 1 das imagens
-        # TODO: Melhorar isso
-        # algum erro com links de imagens, eles ficam com \n no meio aparentemente
-        for img in soup.find_all("img"):
-            src = img.get("src")
-            if src in seen_src:
-                img.decompose()  # Remove as imagens duplicadas
-            else:
-                seen_src.add(src)  # Marca o src como visto 
-        
-
-        # h = title + f"<h1>{_author}</h1>\n" + body 
-        h = f"<h1>Autor: {_author}</h1>" + all.prettify()
-       
-        if all != None:
-            return writeHtml(reddit_url, h)
-        else:
-            return -1
-    except requests.exceptions.RequestException:
-        return -2
-    
-def getHtmlFromMedium(url):
-    try:
-        r = requests.get(url)
-        r.raise_for_status()
-
-        soup = BeautifulSoup(r.text, "html.parser")
-        # TODO: Criar ifs
-        title_ = soup.find("h1", {"data-testid": "storyTitle"})
-        title = title_.prettify() if title_ else "<h1>Sem titulo</h1>"
-
-        subtitle_ = soup.find("h2", {"class": "pw-subtitle-paragraph"})
-        subtitle = subtitle_.prettify() if subtitle_ else "<h3>Sem subtitulo</h3>"
-
-        author_ = soup.find("a", {"data-testid": "authorName"})
-        author = author_.prettify() if author_ else "<h3>Autor Desconhecido</h3>"
-
-        # text_ = soup.find_all("p", {"class": "pw-post-body-paragraph"})
-        # text = " ".join([element.get_text(strip=True) for element in text_])
-        #TODO: Melhorar
-        f = lambda tag: "data-selectable-paragraph" in tag.attrs or tag.name == "pre" or "pw-post-body-paragraph" in tag.get("class", []) or "picture" == tag.name or "figcaption" == tag.name or "h1" == tag.name or tag.name == "ul"
-        text_ = soup.find_all(f)
-        text = " ".join([str(element) for element in text_])
-        r = title + subtitle + author + text
-        return writeHtml(url, r)
-
-    except requests.exceptions.RequestException:
+    soup = getSoap(reddit_url, {}, {})
+    if soup == -1:
         return -1
+
+        # TODO: Pegar videos
+    all = soup.find("shreddit-post")
+    _author = (
+        all.get("author", "Autor Desconhecido") if all else "Autor Desconhecido"
+    )
+    soup.find("div", {"slot": "credit-bar"}).decompose()
+
+    # Elimina elementos que só carregam mais tarde
+    for tag in soup.find_all(attrs={"loading": "lazy"}):
+        tag.decompose()  # Remove a tag do documento
+    seen_src = set()
+    # Ao pegar posts apenas com imagens ele repete as imagens por causa de navegabilidade...
+    # assim basicamente estou deixando apenas 1 das imagens
+    # TODO: Melhorar isso
+    # algum erro com links de imagens, eles ficam com \n no meio aparentemente
+    for img in soup.find_all("img"):
+        src = img.get("src")
+        if src in seen_src:
+            img.decompose()  # Remove as imagens duplicadas
+        else:
+            seen_src.add(src)  # Marca o src como visto
+
+        # h = title + f"<h1>{_author}</h1>\n" + body
+    h = f"<h1>Autor: {_author}</h1>" + all.prettify()
+
+    if all != None:
+        return writeHtml(reddit_url, h)
+    else:
+        return -1
+
+
+def getHtmlFromMedium(url):
+    soup = getSoap(url, {}, {})
+    if soup == -1:
+        return -1
+
+    # TODO: Criar ifs
+    title_ = soup.find("h1", {"data-testid": "storyTitle"})
+    title = title_.prettify() if title_ else "<h1>Sem titulo</h1>"
+
+    subtitle_ = soup.find("h2", {"class": "pw-subtitle-paragraph"})
+    subtitle = subtitle_.prettify() if subtitle_ else "<h3>Sem subtitulo</h3>"
+
+    author_ = soup.find("a", {"data-testid": "authorName"})
+    author = author_.prettify() if author_ else "<h3>Autor Desconhecido</h3>"
+
+    # text_ = soup.find_all("p", {"class": "pw-post-body-paragraph"})
+    # text = " ".join([element.get_text(strip=True) for element in text_])
+    # TODO: Melhorar
+    f = (
+        lambda tag: "data-selectable-paragraph" in tag.attrs
+            or tag.name == "pre"
+            or "pw-post-body-paragraph" in tag.get("class", [])
+            or "picture" == tag.name
+            or "figcaption" == tag.name
+            or "h1" == tag.name
+            or tag.name == "ul"
+    )
+    text_ = soup.find_all(f)
+    text = " ".join([str(element) for element in text_])
+    r = title + subtitle + author + text
+    return writeHtml(url, r)
+
+
 
 def getHtmlFromDevTo(url):
-    try:
-        r = requests.get(url)
-        r.raise_for_status()
-
-        soup = BeautifulSoup(r.text, "html.parser")
-
-        header_ = soup.find("div", {"class": "crayons-article__header__meta"})
-        # pegar apenas o titulo
-        header = header_.find("h1").prettify() if header_ else "<h1>Sem Titulo</h1>"
-        
-        main_ = soup.find("div", {"class": "crayons-article__main"})
-        main = main_.prettify() if main_ else "<h1>Sem main</h1>"
-
-        h = header + main 
-        return writeHtml(url, h)
-    except requests.exceptions.RequestException:
+    soup = getSoap(url, {}, {})
+    if soup == -1:
         return -1
+
+    r = requests.get(url)
+    r.raise_for_status()
+
+    soup = BeautifulSoup(r.text, "html.parser")
+
+    header_ = soup.find("div", {"class": "crayons-article__header__meta"})
+    # pegar apenas o titulo
+    header = header_.find("h1").prettify() if header_ else "<h1>Sem Titulo</h1>"
+
+    main_ = soup.find("div", {"class": "crayons-article__main"})
+    main = main_.prettify() if main_ else "<h1>Sem main</h1>"
+
+    h = header + main
+    return writeHtml(url, h)
 
 
 sites_prefix = ["x.com", "reddit.com", "dev.to", "medium.com"]
 sites_handler = [getHtmlFromX, getHtmlFromReddit, getHtmlFromDevTo, getHtmlFromMedium]
+
 
 def main():
     if len(sys.argv) != 2:
@@ -173,9 +195,8 @@ def main():
     if len(linhas) <= 0:
         print(f"Erro nas linhas de txt, {len(linhas)}")
 
-    processList(linhas, md_name)    
+    processList(linhas, md_name)
     print(f"Arquivo {md_name} criado")
-
 
 
 def processList(lista, md_name):
@@ -186,19 +207,14 @@ def processList(lista, md_name):
         for jdx, j in enumerate(sites_prefix):
             if j in i:
                 h = sites_handler[jdx](i)
+                saveMd(h, md_name, f"***\n# {idx}\n")
 
-        if len(h) == 0:
-            continue
-
-        saveMd(h, md_name, f"***\n# {idx}\n")
-
-        try: 
-            if isinstance(h, str):
-                os.remove(h)
-        except:
-            print(f"erro ao remover arquivo {h}")
+                try:
+                    if isinstance(h, str):
+                        os.remove(h)
+                except Exception:
+                    print(f"erro ao remover arquivo {h}")
 
 
-
-if __name__ == "__main__": 
-    main() 
+if __name__ == "__main__":
+    main()
